5693c7ce7095a7c1bff719c19920e7a8c21fc3bf
Bene <benediktschnupp@me.com>
2026-01-19 12:34:12 +0100

    feat: add `/about`, `/work`, and `/contact` pages to the sitemap.

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -9,11 +9,29 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url>
+    <url>
         <loc>${baseUrl}</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>1.0</priority>
+      </url>
+      <url>
+        <loc>${baseUrl}/about</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+      </url>
+      <url>
+        <loc>${baseUrl}/work</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
+      </url>
+      <url>
+        <loc>${baseUrl}/contact</loc>
+        <lastmod>${new Date().toISOString()}</lastmod>
+        <changefreq>monthly</changefreq>
+        <priority>0.8</priority>
       </url>
       ${projects
         .map((project) => {
